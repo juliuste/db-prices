12750ca9262431c0f762bdd76a1fb3cd26598320
Jannis R <mail@jannisr.de>
2017-05-29 00:45:23 +0200

    refactor parsing

--- index.js
+++ index.js
@@ -1,87 +1,13 @@
 'use strict'
 
+const moment = require('moment-timezone')
 const {stringify} = require('query-string')
 const {fetch} = require('fetch-ponyfill')()
-const moment = require('moment-timezone')
-const slugg = require('slugg')
-const minBy = require('lodash.minby')
+
+const parse = require('./parse')
 
 const formatDate = (date) => moment(date).tz('Europe/Berlin').format('DD.MM.YY')
 const formatTime = (date) => moment(date).tz('Europe/Berlin').format('HH:mm')
-
-const parsePrice = (string) => parseFloat(string.replace(',', '.'))
-
-const parseOffer = (data) => ({
-	// todo: sel, t, c, arq, ff, aix, risids
-	ref: data.pky,
-	discount: data.tt === 'SP', // are there others than SP & NP?
-	price: parsePrice(data.p),
-	routes: data.sids,
-	anyTrain: data.zb !== 'Y'
-})
-
-const parseWhen = (data) => {
-	// todo: timezone
-	return moment.tz(parseInt(data.m), 'Europe/Berlin').toISOString()
-}
-
-const parseLeg = (data) => ({
-	// todo: rp, re, sp
-	origin: {
-		type: 'station',
-		id: data.s,
-		name: data.sn
-		// todo: coordinates
-	},
-	start: parseWhen(data.dep),
-	departurePlatform: data.pd,
-	to: {
-		type: 'station',
-		id: data.d,
-		name: data.dn
-		// todo: coordinates
-	},
-	end: parseWhen(data.arr),
-	arrivalPlatform: data.pa,
-	line: {
-		type: 'line',
-		id: slugg(data.tn),
-		name: data.tn,
-		// todo: mode
-		product: data.eg
-	}
-})
-
-const parseJourney = (_, offers) => {
-	let offer = minBy(offers.filter((o) => o.routes.includes(_.sid)), (o) => o.price)
-	const price = offer ? {
-		currency: 'EUR',
-		amount: offer.price,
-		discount: offer.discount
-	} : null
-
-	return {
-		// todo: sel, dir
-		type: 'journey',
-		id: _.sid,
-		legs: _.trains.map(parseLeg),
-		price,
-		nightTrain: _.NZVerb // todo: why here?
-	}
-}
-
-const parseNotes = (data) => {
-	const results = {}
-	for(let ref in data){
-		results[ref] = parseNote(data[ref])
-	}
-	return results
-}
-
-const parseNote = (data) => ({
-	title: data.name,
-	text: data.hinweis
-})
 
 const defaults = {
 	class: 2, // 1st class or 2nd class?
@@ -99,7 +25,7 @@ const defaults = {
 
 const endpoint = 'http://ps.bahn.de/preissuche/preissuche/psc_service.go'
 
-const prices = (start, dest, date, opt) => {
+const queryPrices = (start, dest, date, opt) => {
 	opt = Object.assign({}, defaults, opt || {})
 	date = date || new Date()
 
@@ -139,18 +65,20 @@ const prices = (start, dest, date, opt) => {
 		return res.json()
 	})
 	.then((body) => {
+		const notes = parse.notes(body.peTexte)
+
 		const offers = []
 		for (let id in body.angebote) {
-			offers.push(parseOffer(body.angebote[id]))
+			offers.push(parse.offer(body.angebote[id], notes))
 		}
 
 		const journeys = {}
 		for (let id in body.verbindungen) {
-			journeys[id] = parseJourney(body.verbindungen[id], offers)
+			journeys[id] = parse.journey(body.verbindungen[id], offers)
 		}
 
 		return journeys
 	})
 }
 
-module.exports = prices
+module.exports = queryPrices
---
+++ parse.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const moment = require('moment-timezone')
+const slugg = require('slugg')
+const minBy = require('lodash.minby')
+
+const price = (string) => parseFloat(string.replace(',', '.'))
+
+const offer = (data, notes) => {
+	const offer = {
+		// todo: sel, t, c, arq, ff, aix, risids
+		name: null,
+		description: null,
+		discount: data.tt === 'SP', // are there others than SP & NP?
+		price: price(data.p),
+		routes: data.sids,
+		anyTrain: data.zb !== 'Y'
+	}
+
+	if (notes[data.pky]) Object.assign(offer, notes[data.pky])
+
+	return offer
+}
+
+const when = (data) => {
+	// todo: timezone
+	return moment.tz(parseInt(data.m), 'Europe/Berlin').toISOString()
+}
+
+const leg = (data) => ({
+	// todo: rp, re, sp
+	origin: {
+		type: 'station',
+		id: data.s,
+		name: data.sn
+		// todo: coordinates
+	},
+	start: when(data.dep),
+	departurePlatform: data.pd,
+	to: {
+		type: 'station',
+		id: data.d,
+		name: data.dn
+		// todo: coordinates
+	},
+	end: when(data.arr),
+	arrivalPlatform: data.pa,
+	line: {
+		type: 'line',
+		id: slugg(data.tn),
+		name: data.tn,
+		// todo: mode
+		product: data.eg
+	}
+})
+
+const journey = (_, offers) => {
+	let offer = minBy(offers.filter((o) => o.routes.includes(_.sid)), (o) => o.price)
+	const price = offer ? {
+		currency: 'EUR',
+		amount: offer.price,
+		discount: offer.discount,
+		name: offer.name,
+		description: offer.description
+	} : null
+
+	return {
+		// todo: sel, dir
+		type: 'journey',
+		id: _.sid,
+		legs: _.trains.map(leg),
+		price,
+		nightTrain: _.NZVerb // todo: why here?
+	}
+}
+
+const notes = (data) => {
+	const notes = {}
+	for (let ref in data) {
+		notes[ref] = {name: data[ref].name, description: data[ref].hinweis}
+	}
+	return notes
+}
+
+module.exports = {notes, offer, journey}
